f24912275410234e1ec927a424764801aa22adcc
Mateus Devino <mdevino@ibm.com>
2024-11-04 07:27:44 -0300

    test: standalone chat with content text message
    Signed-off-by: Mateus Devino <mdevino@ibm.com>

--- tests/chat.rs
+++ tests/chat.rs
@@ -62,3 +62,44 @@ async fn test_chat_detection_with_string_message() {
         ]
     }));
 }
+
+#[traced_test]
+#[tokio::test]
+async fn test_chat_detection_with_text_content_message() {
+    // Initialize orchestrator
+    let shared_state = ONCE.get_or_init(shared_state).await.clone();
+    let server = TestServer::new(get_app(shared_state)).unwrap();
+
+    // make orchestrator request
+    let response = server
+        .post(ORCHESTRATOR_CHAT_DETECTION_ENDPOINT)
+        .json(&json!({
+            "messages": [
+                {
+                    "content": [
+                      {
+                        "type": "text",
+                        "text": "This is the content of the message"
+                      }
+                    ],
+                    "role": "user"
+                }
+            ],
+            "detectors": {
+                MOCKED_DETECTOR_NAME: {}
+            }
+        }))
+        .await;
+
+    // assertions
+    response.assert_status(StatusCode::OK);
+    response.assert_json(&json!({
+        "detections": [
+            {
+                "detection_type": "is_message_valid",
+                "detection": "Yes",
+                "score": 1.0
+            }
+        ]
+    }));
+}

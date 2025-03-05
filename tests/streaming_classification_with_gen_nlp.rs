0f4a7c9a1ec0939c6ba7d2aca2f8454f947c71b8
Mateus Devino <mdevino@ibm.com>
2025-03-05 11:18:03 -0300

    test case: test_output_detector_sentence_chunker_with_detections()
    Signed-off-by: Mateus Devino <mdevino@ibm.com>

--- tests/streaming_classification_with_gen_nlp.rs
+++ tests/streaming_classification_with_gen_nlp.rs
@@ -1424,3 +1424,223 @@ async fn test_output_detector_sentence_chunker_no_detections() -> Result<(), any
 
     Ok(())
 }
+
+/// Asserts that the generated text is returned alongside detections when an output detector
+/// configured with a sentence chunker finds detections.
+#[test(tokio::test)]
+async fn test_output_detector_sentence_chunker_with_detections() -> Result<(), anyhow::Error> {
+    let detector_name = DETECTOR_NAME_ANGLE_BRACKETS_SENTENCE;
+
+    // Add generation mock
+    let model_id = "my-super-model-8B";
+    let mut headers = HeaderMap::new();
+    headers.insert(
+        GENERATION_NLP_MODEL_ID_HEADER_NAME,
+        model_id.parse().unwrap(),
+    );
+
+    let mut generation_mocks = MockSet::new();
+    generation_mocks.insert(
+        MockPath::post(GENERATION_NLP_STREAMING_ENDPOINT),
+        Mock::new(
+            MockRequest::pb(ServerStreamingTextGenerationTaskRequest {
+                text: "Hi there! How are you?".into(),
+                ..Default::default()
+            })
+            .with_headers(headers.clone()),
+            MockResponse::pb_stream(vec![
+                GeneratedTextStreamResult {
+                    generated_text: "I".into(),
+                    ..Default::default()
+                },
+                GeneratedTextStreamResult {
+                    generated_text: " am".into(),
+                    ..Default::default()
+                },
+                GeneratedTextStreamResult {
+                    generated_text: " great!".into(),
+                    ..Default::default()
+                },
+                GeneratedTextStreamResult {
+                    generated_text: " What".into(),
+                    ..Default::default()
+                },
+                GeneratedTextStreamResult {
+                    generated_text: " about".into(),
+                    ..Default::default()
+                },
+                GeneratedTextStreamResult {
+                    generated_text: " <you>?".into(),
+                    ..Default::default()
+                },
+            ]),
+        ),
+    );
+
+    // Add output chunker mock
+    let chunker_id = CHUNKER_NAME_SENTENCE;
+    let mut chunker_headers = HeaderMap::new();
+    chunker_headers.insert(CHUNKER_MODEL_ID_HEADER_NAME, chunker_id.parse()?);
+
+    let mut chunker_mocks = MockSet::new();
+    chunker_mocks.insert(
+        MockPath::post(CHUNKER_STREAMING_ENDPOINT),
+        Mock::new(
+            MockRequest::pb_stream(vec![
+                BidiStreamingChunkerTokenizationTaskRequest {
+                    text_stream: "I".into(),
+                    input_index_stream: 0,
+                },
+                BidiStreamingChunkerTokenizationTaskRequest {
+                    text_stream: " am".into(),
+                    input_index_stream: 1,
+                },
+                BidiStreamingChunkerTokenizationTaskRequest {
+                    text_stream: " great!".into(),
+                    input_index_stream: 2,
+                },
+                BidiStreamingChunkerTokenizationTaskRequest {
+                    text_stream: " What".into(),
+                    input_index_stream: 3,
+                },
+                BidiStreamingChunkerTokenizationTaskRequest {
+                    text_stream: " about".into(),
+                    input_index_stream: 4,
+                },
+                BidiStreamingChunkerTokenizationTaskRequest {
+                    text_stream: " <you>?".into(),
+                    input_index_stream: 5,
+                },
+            ])
+            .with_headers(chunker_headers),
+            MockResponse::pb_stream(vec![
+                ChunkerTokenizationStreamResult {
+                    results: vec![Token {
+                        start: 0,
+                        end: 11,
+                        text: "I am great!".into(),
+                    }],
+                    token_count: 0,
+                    processed_index: 11,
+                    start_index: 0,
+                    input_start_index: 0,
+                    input_end_index: 0,
+                },
+                ChunkerTokenizationStreamResult {
+                    results: vec![Token {
+                        start: 11,
+                        end: 29,
+                        text: " What about <you>?".into(),
+                    }],
+                    token_count: 0,
+                    processed_index: 29,
+                    start_index: 11,
+                    input_start_index: 0,
+                    input_end_index: 0,
+                },
+            ]),
+        ),
+    );
+
+    // Add output detection mock
+    let mut detection_mocks = MockSet::new();
+    detection_mocks.insert(
+        MockPath::post(TEXT_CONTENTS_DETECTOR_ENDPOINT),
+        Mock::new(
+            MockRequest::json(ContentAnalysisRequest {
+                contents: vec!["I am great!".into()],
+                detector_params: DetectorParams::new(),
+            }),
+            MockResponse::json([Vec::<ContentAnalysisResponse>::new()]),
+        ),
+    );
+    detection_mocks.insert(
+        MockPath::post(TEXT_CONTENTS_DETECTOR_ENDPOINT),
+        Mock::new(
+            MockRequest::json(ContentAnalysisRequest {
+                contents: vec![" What about <you>?".into()],
+                detector_params: DetectorParams::new(),
+            }),
+            MockResponse::json([vec![ContentAnalysisResponse {
+                start: 12,
+                end: 15,
+                text: "you".into(),
+                detection: "has_angle_brackets".into(),
+                detection_type: "angle_brackets".into(),
+                detector_id: Some(detector_name.into()),
+                score: 1.0,
+                evidence: None,
+            }]]),
+        ),
+    );
+
+    // Start orchestrator server and its dependencies
+    let mock_chunker_server = GrpcMockServer::new(chunker_id, chunker_mocks)?;
+    let mock_detector_server = HttpMockServer::new(detector_name, detection_mocks)?;
+    let generation_server = GrpcMockServer::new("nlp", generation_mocks)?;
+    let orchestrator_server = TestOrchestratorServer::run(
+        ORCHESTRATOR_CONFIG_FILE_PATH,
+        find_available_port().unwrap(),
+        find_available_port().unwrap(),
+        Some(generation_server),
+        None,
+        Some(vec![mock_detector_server]),
+        Some(vec![mock_chunker_server]),
+    )
+    .await?;
+
+    // Example orchestrator request with streaming response
+    let response = orchestrator_server
+        .post(ORCHESTRATOR_STREAMING_ENDPOINT)
+        .json(&GuardrailsHttpRequest {
+            model_id: model_id.into(),
+            inputs: "Hi there! How are you?".into(),
+            guardrail_config: Some(GuardrailsConfig {
+                input: None,
+                output: Some(GuardrailsConfigOutput {
+                    models: HashMap::from([(detector_name.into(), DetectorParams::new())]),
+                }),
+            }),
+            text_gen_parameters: None,
+        })
+        .send()
+        .await?;
+
+    debug!("{response:#?}");
+
+    // Test custom SseStream wrapper
+    let sse_stream: SseStream<ClassifiedGeneratedTextStreamResult> =
+        SseStream::new(response.bytes_stream());
+    let messages = sse_stream
+        .collect::<Vec<_>>()
+        .await
+        .into_iter()
+        .collect::<Result<Vec<_>, anyhow::Error>>()?;
+    debug!("{messages:#?}");
+
+    assert!(messages.len() == 2);
+
+    assert!(messages[0].generated_text == Some("I am great!".into()));
+    assert!(messages[0].token_classification_results.output == Some(vec![]));
+    assert!(messages[0].start_index == Some(0));
+    assert!(messages[0].processed_index == Some(11));
+
+    assert!(messages[1].generated_text == Some(" What about <you>?".into()));
+    assert!(
+        messages[1].token_classification_results.output
+            == Some(vec![TokenClassificationResult {
+                start: 12,
+                end: 15,
+                word: "you".into(),
+                entity: "has_angle_brackets".into(),
+                entity_group: "angle_brackets".into(),
+                detector_id: Some(detector_name.into()),
+                score: 1.0,
+                token_count: None
+            }])
+    );
+    assert!(messages[1].start_index == Some(11));
+    assert!(messages[1].processed_index == Some(29));
+
+    Ok(())
+}
